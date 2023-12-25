aa2d31c20f2a0e512148b64a3485335ddc51169c
Rachid Eddaha <rachid.eddaha@antaresvision.com>
2023-12-25 23:01:21 +0100

    add service according to the task (pdf)

---
+++ internal/service/models.go
@@ -0,0 +1,28 @@
+package service
+
+// to separate the entity for the repository from the dto of the request
+
+type AddTask struct {
+	Status string
+}
+
+type GetTask struct {
+	ID int
+	Task
+}
+
+type AddResponse struct {
+	ID             int
+	Status         string
+	HTTPStatusCode int
+	Headers        map[string][]string
+	Length         int
+}
+
+type Task struct {
+	Status         string
+	HTTPStatusCode int
+	Headers        map[string][]string
+	Length         int
+	Error          string // this field used only to keep track of the error message
+}
